11c02ed3df57727792f912c57987d0da487ae06a
i30101 <andrewkim229@gmail.com>
2024-08-24 14:53:40 -0400

    datatable

--- src/data/datatable.py
+++ src/data/datatable.py
@@ -12,8 +12,6 @@ Data viewing table
 import tkinter as tk
 from tkinter import ttk
 
-from src.variables import Variables
-
 from src.data.databox import DataBox
 
 class DataTable(DataBox):
@@ -38,62 +36,68 @@ class DataTable(DataBox):
         self.frame_number_entry = ttk.Entry(self.box, textvariable=self.frame_number, width=self.ENTRY_WIDTH)
         self.frame_number_entry.grid(row=0, column=1, padx=0, pady=5, sticky="ew")
 
-        # # ramp number
-        self.ramp = tk.IntVar()
-        self.ramp_label = ttk.Label(self.box, text="Ramp number", width=self.LABEL_WIDTH)
-        self.ramp_label.grid(row=0, column=2, padx=(30, 0), pady=5, sticky="w")
-        self.ramp_entry = ttk.Entry(self.box, textvariable=self.ramp, state="readonly", width=self.ENTRY_WIDTH)
-        self.ramp_entry.grid(row=0, column=3, padx=0, pady=5, sticky="ew")
+        # number of frames
+        self.frames = tk.IntVar()
+        self.frames_label = ttk.Label(self.box, text="Frames", width=self.LABEL_WIDTH)
+        self.frames_label.grid(row=0, column=2, padx=(30, 0), pady=5, sticky="w")
+        self.frames_entry = ttk.Entry(self.box, textvariable=self.frames, state="readonly", width=self.ENTRY_WIDTH)
+        self.frames_entry.grid(row=0, column=3, padx=0, pady=5, sticky="ew")
 
         # temperature
         self.temperature = tk.DoubleVar()
-        self.temperature_label = ttk.Label(self.box, text="Temperature", width=self.LABEL_WIDTH)
+        self.temperature_label = ttk.Label(self.box, text="Temp (°C)", width=self.LABEL_WIDTH)
         self.temperature_label.grid(row=1, column=0, padx=0, pady=5, sticky="w")
         self.temperature_entry = ttk.Entry(self.box, textvariable=self.temperature, state="readonly", width=self.ENTRY_WIDTH)
         self.temperature_entry.grid(row=1, column=1, padx=0, pady=5, sticky="ew")
 
         # temperature limit
         self.limit = tk.DoubleVar()
-        self.limit_label = ttk.Label(self.box, text="Limit", width=self.LABEL_WIDTH)
+        self.limit_label = ttk.Label(self.box, text="Limit (°C)", width=self.LABEL_WIDTH)
         self.limit_label.grid(row=1, column=2, padx=(30, 0), pady=5, sticky="w")
         self.limit_entry = ttk.Entry(self.box, textvariable=self.limit, state="readonly", width=self.ENTRY_WIDTH)
         self.limit_entry.grid(row=1, column=3, padx=0, pady=5, sticky="ew")
 
         # temperature rate
         self.rate = tk.DoubleVar()
-        self.rate_label = ttk.Label(self.box, text="Rate", width=self.LABEL_WIDTH)
+        self.rate_label = ttk.Label(self.box, text="Rate (°C/min)", width=self.LABEL_WIDTH)
         self.rate_label.grid(row=2, column=0, padx=0, pady=5, sticky="w")
-        self.rate_entry = ttk.Entry(self.box, textvariable=self.limit, state="readonly", width=self.ENTRY_WIDTH)
+        self.rate_entry = ttk.Entry(self.box, textvariable=self.rate, state="readonly", width=self.ENTRY_WIDTH)
         self.rate_entry.grid(row=2, column=1, padx=0, pady=5, sticky="ew")
 
+        # ramp number
+        self.ramp = tk.IntVar()
+        self.ramp_label = ttk.Label(self.box, text="Ramp", width=self.LABEL_WIDTH)
+        self.ramp_label.grid(row=2, column=2, padx=(30, 0), pady=5, sticky="w")
+        self.ramp_entry = ttk.Entry(self.box, textvariable=self.ramp, state="readonly", width=self.ENTRY_WIDTH)
+        self.ramp_entry.grid(row=2, column=3, padx=0, pady=5, sticky="ew")
 
-    def set_data(self, frame_number: int, ramp: int, temperature: float, limit: float, rate: float):
+
+
+    def set_data(self,
+                 frame_number: int,
+                 frames: int,
+                 temperature: float,
+                 limit: float,
+                 rate: float,
+                 ramp: int ):
         """ Sets the data in the entry boxes """
         # Only update frame_number if entry does not have focus
         if self.frame_number_entry.focus_get() != self.frame_number_entry:
             self.frame_number.set(frame_number)
-        self.ramp.set(ramp)
+        self.frames.set(frames)
         self.temperature.set(temperature)
         self.limit.set(limit)
         self.rate.set(rate)
+        self.ramp.set(ramp)
 
         # set all entries to readonly
-        self.ramp_entry.config(state="readonly")
+        self.frames_entry.config(state="readonly")
         self.temperature_entry.config(state="readonly")
         self.limit_entry.config(state="readonly")
         self.rate_entry.config(state="readonly")
+        self.ramp_entry.config(state="readonly")
 
 
     def clear_data(self):
         """ Clears the data in the entry boxes """
-        self.frame_number.set(0)
-        self.ramp.set(0)
-        self.temperature.set(0.0)
-        self.limit.set(0.0)
-        self.rate.set(0.0)
-
-        # set all entries to readonly
-        self.ramp_entry.config(state="readonly")
-        self.temperature_entry.config(state="readonly")
-        self.limit_entry.config(state="readonly")
-        self.rate_entry.config(state="readonly")
+        self.set_data(0, 0, 0.0, 0.0, 0.0, 0)
